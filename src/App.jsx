1bb1f24ad712fbe9ede87b8089f385a6b2ad7dea
Victor Lupascu <sinajeen@gmail.com>
2021-09-13 15:39:01 -0500

    create about and nav comp

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import About from './components/About'
+import Nav from './components/Nav'
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <main>
+        <About />
+      </main>
+    </div>
+  )
+}
+
+export default App
